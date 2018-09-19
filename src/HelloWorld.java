b7c223a8d4edde2d6b9e61b42192d19229f88c9a
Yevhenii <Yevhenii@Yevhenii>
2018-09-19 09:32:49 +0400

    10x git je super
    + for-cyklus

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Git je super");
+		}
 	}
 }
