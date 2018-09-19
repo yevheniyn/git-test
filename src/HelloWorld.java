a243e85a69fec7260f950e810901ec2abe1a30e9
student <student@158.197.35.98>
2018-09-19 08:45:30 +0200

    final commit
    finalna verzia

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,5 @@
-
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Git je super");
-		}
+		System.out.println("Zmena commit");
 	}
 }
